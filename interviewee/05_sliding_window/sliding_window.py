0ec08c5f21aba3137322e7412705d5205552e183
Jain, Anshul <Anshul.Jain@Clarivate.com>
2021-06-29 21:09:43 +0530

    Handled edge cases for sliding_window

--- interviewee/05_sliding_window/sliding_window.py
+++ interviewee/05_sliding_window/sliding_window.py
@@ -1,18 +1,25 @@
-# Question: In a given list of n integers, find the 'max sum' of 'k' consecutive integers with a solution with O(n) complexity
+# Question: In a given list of n integers, find the 'max sum' of
+# 'k' consecutive integers with a solution with O(n) complexity
 
 def max_sum(arr, k):
     n = len(arr)
 
-    max_sum = sum(arr[:k])
-    for i in range(0, n-k):
-        new_sum = max_sum - arr[i] + arr[i+k]
-        max_sum = max([max_sum, new_sum])
+    # perform the calculations only if n > k:
+    if n > k:
+        window_sum = sum(arr[:k])
+        max_sum = window_sum
+        for i in range(0, n-k):
+            window_sum = window_sum - arr[i] + arr[i+k]            
+            max_sum = max([max_sum, window_sum])            
+    else:
+        return 'Invalid!!. The window cannot be greater than array length.'
 
     return max_sum
 
 
 if __name__ == '__main__':
     my_list = [80, -50, 90, 100]
-    my_sum = max_sum(my_list, 2)
+    k = 1
+    my_sum = max_sum(my_list, k)
 
     print(my_sum)

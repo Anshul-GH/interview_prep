ba88947f31d3b60320839dff955246c2e4e7aecc
Anshul <samgalacestyle01@gmail.com>
2021-07-01 02:08:53 +0530

    boats_to_save problem and folter rearrange

--- interviewee/00_misc_questions/move_zeros.py
+++ interviewee/00_misc_questions/move_zeros.py
@@ -1,3 +1,10 @@
+# write a function to move all the '0' elements
+# within a list to the end while maintaining the 
+# order of occourance for remaining elements
+# Example: [0, 1, 0, 3, 12] --> [1, 3, 12, 0, 0]
+
+# Given a list/array, move all the zeros within the array to the end maintaining the relative order of the other elements.
+
 def move_zeros(arr):
     # non_zero_counter
     counter = 0

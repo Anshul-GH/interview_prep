0300d0d5ddad437e83737bf25382555599ec3c86
AJ <samgalacestyle01@gmail.com>
2021-07-11 22:00:31 +0530

    Routine Commit

--- interviewee/00_misc_questions/container_with_most_water.py
+++ interviewee/00_misc_questions/container_with_most_water.py
@@ -9,29 +9,46 @@
 # Explanation: The above vertical lines are represented by array [1,8,6,2,5,4,8,3,7]. 
 # In this case, the max area of water the container can contain is 49.
 
-def max_area_container(arr):
-    left = 0
-    right = len(arr)-1
-    max_area = 0
+# my solution (not working)
+# def max_area_container(arr):
+#     left = 0
+#     right = len(arr)-1
+#     max_area = 0
 
-    while left < right:
-        len_x = right - left
-        len_y = min(arr[left], arr[right])
+#     while left < right:
+#         len_x = right - left
+#         len_y = min(arr[left], arr[right])
 
-        area = len_x * len_y
+#         area = len_x * len_y
 
-        max_area = max(max_area, area)
+#         max_area = max(max_area, area)
 
-        if (arr[left+1] - arr[left]) > (arr[right-1] - arr[right]):
-            left += 1
+#         if (arr[left+1] - arr[left]) >= (arr[right-1] - arr[right]):
+#             left += 1
+#         else:
+#             right -= 1
+
+#     return max_area
+
+# working solution
+def max_area_container(height):
+    maxarea = 0
+    l = 0
+    r = len(height)-1
+
+    while(l<r):
+        maxarea = max(maxarea, min(height[l],height[r])*(r-l))
+        if(height[l]<height[r]):
+            l+=1
         else:
-            right -= 1
-
-    return max_area
+            r-=1
+    return maxarea
 
 
 if __name__ == '__main__':
-    my_arr = [1,8,6,2,5,4,8,3,7]
+    my_arr = [1,8,6,2,5,4,8,25,7]
+    # [1,2,4,3]
+    # [1,8,6,2,5,4,8,3,7]
     max_area = max_area_container(my_arr)
 
     print(max_area)

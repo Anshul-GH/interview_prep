4782955213b4783364da6e32815cad349bce1d76
Jain, Anshul <Anshul.Jain@Clarivate.com>
2021-08-20 22:36:23 +0530

    Routine Commit

--- interviewee/01_recursion/02_fibonacci_generators.py
+++ interviewee/01_recursion/02_fibonacci_generators.py
@@ -1,8 +1,8 @@
 def fibonacci_sequence():
-    a,b = 0,1
+    a, b = 0, 1
     while True:
         yield a
-        a,b = b, a+b
+        a, b = b, a+b
 
 generator = fibonacci_sequence()
 for i in range(10):

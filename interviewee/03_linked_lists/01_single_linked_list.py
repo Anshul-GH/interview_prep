cb4d69461e1c3f4b92bc4bd41f3760dd6a1bdb22
Anshul Jain <samgalacestyle01@gmail.com>
2021-12-09 17:14:24 +0530

    Linked List related implementations

--- interviewee/03_linked_lists/01_single_linked_list.py
+++ interviewee/03_linked_lists/01_single_linked_list.py
@@ -9,10 +9,13 @@ class SingleLinkedList:
         self.head = None
 
     def traverse_list(self):
-        print_node = self.head
-        while print_node is not None:
-            print(print_node.value)
-            print_node = print_node.next
+        if self.head is None:
+            print('List is empty.')
+        else:
+            print_node = self.head
+            while print_node is not None:
+                print(print_node.value)
+                print_node = print_node.next
     
     def add_node_begining(self, value):
         new_node = Node(value=value)
@@ -24,9 +27,21 @@ class SingleLinkedList:
         new_node = Node(value=value)
         last = self.head.next
         while last.next is not None:
-            last = last.next
-        
+            last = last.next        
         last.next = new_node
+
+    def delete_node(self, value):
+        if self.head.value == value:
+            self.head = self.head.next
+        else:
+            previous_node = self.head
+            current_node = self.head
+
+            while current_node.value != value:
+                previous_node = current_node
+                current_node = current_node.next
+        
+            previous_node.next = current_node.next
 
 
 singleLinkedList = SingleLinkedList()
@@ -38,10 +53,27 @@ singleLinkedList.head.next = node2
 
 print('Original List:')
 singleLinkedList.traverse_list()
+
 print('Appended List:')
 print('At the begning:')
 singleLinkedList.add_node_begining(0)
 singleLinkedList.traverse_list()
 print('At the end:')
 singleLinkedList.add_node_end(3)
+singleLinkedList.traverse_list()
+
+print('Delete a node (2):')
+singleLinkedList.delete_node(2)
+singleLinkedList.traverse_list()
+
+print('Delete a node (0):')
+singleLinkedList.delete_node(0)
+singleLinkedList.traverse_list()
+
+print('Delete a node (3):')
+singleLinkedList.delete_node(3)
+singleLinkedList.traverse_list()
+
+print('Delete a node (1):')
+singleLinkedList.delete_node(1)
 singleLinkedList.traverse_list()

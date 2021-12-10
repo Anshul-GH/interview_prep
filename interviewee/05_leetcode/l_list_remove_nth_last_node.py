f4b342daa34997ccaf3128dd1be48d1788600a6b
Anshul Jain <samgalacestyle01@gmail.com>
2021-12-10 23:41:16 +0530

    Delete nth node from the end within a linked list

--- interviewee/05_leetcode/l_list_remove_nth_last_node.py
+++ interviewee/05_leetcode/l_list_remove_nth_last_node.py
@@ -28,25 +28,66 @@ Constraints:
 
 Follow up: Could you do this in one pass?
 '''
+# class Solution:
+#     def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
+#         counter = 0
+#         current_node = head
+
+#         while current_node.next is not None:
+#             current_node = current_node.next
+#             counter += 1
+        
+#         position = counter - n
+#         count = 0
+#         previous_node = head
+#         current_node = head
+
+#         while count <= position:
+#             previous_node = current_node
+#             current_node = current_node.next
+#             count += 1
+        
+#         previous_node.next = current_node.next
+
+#         return head
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
 class Solution:
     def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
-        counter = 0
+        # reduce nth position for better readibility
+        # n = 0 means last node of the list (instead of n = 1)        
+        n = n-1
+        cntr = 0
+        delete_node = None
         current_node = head
-
-        while current_node.next is not None:
+        previous_node = None
+        while current_node:
+            if current_node.next:
+                previous_node = current_node
             current_node = current_node.next
-            counter += 1
-        
-        position = counter - n
-        count = 0
-        previous_node = head
-        current_node = head
+            
+            if cntr == n:                
+                delete_node = head                
+            elif cntr > n:                
+                delete_node = delete_node.next
+            
+            cntr += 1
+       
+        if delete_node.next:
+            delete_node.val = delete_node.next.val
+            delete_node.next = delete_node.next.next
+        # if the list contains only 1 element and
+        # that 1 element needs to be deleted
+        elif n == 0 and cntr == 1:
+            head = None
+        # if its the last node needs to be deleted 
+        # and list has more than 1 element
+        elif n == 0 and previous_node:
+            previous_node.next = None
 
-        while count <= position:
-            previous_node = current_node
-            current_node = current_node.next
-            count += 1
-        
-        previous_node.next = current_node.next
 
         return head

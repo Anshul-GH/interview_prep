326614eea21235550a8d698e4533a8859e3e739e
Anshul Jain <samgalacestyle01@gmail.com>
2021-12-15 20:23:05 +0530

    Grid Traveller problem using dynamic programming

--- interviewee/08_dynamic_programming/grid_traveller.py
+++ interviewee/08_dynamic_programming/grid_traveller.py
@@ -22,5 +22,18 @@ def possible_routes(m, n, routes={}):
         return 1
     elif m == 0 or n == 0:
         return 0
-    
-    return possible_routes(m-1,n) + possible_routes(m,n-1)
+    # are the coordinates already have a calculated route
+    key = str(m) + ',' + str(n)
+    key_inv = str(n) + ',' + str(m)
+    if key in routes:
+        return routes[key]
+    elif key_inv in routes:
+        return routes[key_inv]
+    else:
+        routes[key] = possible_routes(m-1, n, routes) + possible_routes(m, n-1, routes)
+        return routes[key]
+
+if __name__ == "__main__":
+    m = int(input("Enter number of rows(m):"))
+    n = int(input("Enter number of columns(n):"))
+    print(possible_routes(m,n))

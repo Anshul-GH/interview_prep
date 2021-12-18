63907381df6dde6f4580a3cd2f30035696d4cdb4
Anshul Jain <samgalacestyle01@gmail.com>
2021-12-18 21:50:18 +0530

    Implemented converting roman to integer.

--- interviewee/05_leetcode/math_roman_to_integer.py
+++ interviewee/05_leetcode/math_roman_to_integer.py
@@ -11,17 +11,18 @@ C             100
 D             500
 M             1000
 
-For example, 2 is written as II in Roman numeral, just two one's added together. 12 is written as XII, which is simply X + II. The number 27 is written as XXVII, which is XX + V + II.
-
-Roman numerals are usually written largest to smallest from left to right. However, the numeral for four is not IIII. Instead, the number four is written as IV. Because the one is before the five we subtract it making four. The same principle applies to the number nine, which is written as IX. There are six instances where subtraction is used:
+For example, 2 is written as II in Roman numeral, just two one's added together. 12 is written as XII,
+which is simply X + II. The number 27 is written as XXVII, which is XX + V + II.
+Roman numerals are usually written largest to smallest from left to right. However, the numeral for four
+is not IIII. Instead, the number four is written as IV. Because the one is before the five we subtract it
+making four. The same principle applies to the number nine, which is written as IX. There are six
+instances where subtraction is used:
 
     I can be placed before V (5) and X (10) to make 4 and 9. 
     X can be placed before L (50) and C (100) to make 40 and 90. 
     C can be placed before D (500) and M (1000) to make 400 and 900.
 
 Given a roman numeral, convert it to an integer.
-
- 
 
 Example 1:
 Input: s = "III"
@@ -43,3 +44,35 @@ Constraints:
     s contains only the characters ('I', 'V', 'X', 'L', 'C', 'D', 'M').
     It is guaranteed that s is a valid roman numeral in the range [1, 3999].
 '''
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        mapping = {
+                'I': 1,
+                'V': 5,
+                'X': 10,
+                'L': 50,
+                'C': 100,
+                'D': 500,
+                'M': 1000,
+                'IV': 4,
+                'IX': 9,
+                'XL': 40,
+                'XC': 90,
+                'CD': 400,
+                'CM': 900
+
+            }
+        intg = 0
+        counter = 0
+        while counter < len(s):    
+            key = s[counter:counter+2]        
+            if key in mapping:            
+                intg += mapping[key]
+                counter += 2
+            else:
+                key = s[counter]
+                if key in mapping:                
+                    intg += mapping[key]
+                    counter += 1
+
+        return intg

ef27ef0bb2283d87ee602a41c3c41d61b91f4760
Anshul Jain <samgalacestyle01@gmail.com>
2021-12-22 16:14:15 +0530

    Solved the hamming distance problem

--- interviewee/05_leetcode/math_hamming_distance.py
+++ interviewee/05_leetcode/math_hamming_distance.py
@@ -17,5 +17,34 @@ Input: x = 3, y = 1
 Output: 1
 
 Constraints:
-    0 <= x, y <= 231 - 1
+    0 <= x, y <= 2^31 - 1
 '''
+class Solution:
+    def decimal_to_binary(self, num):
+        binary = ''
+        while num >= 1:
+            binary += str(num%2)
+            num = num//2
+        return binary
+
+    def hammingDistance(self, x: int, y: int) -> int:
+        x_bin = self.decimal_to_binary(x)
+        y_bin = self.decimal_to_binary(y)
+
+        h_dist = 0
+        if len(x_bin) < len(y_bin):
+            for idx, bit in enumerate(x_bin):
+                if x_bin[idx] != y_bin[idx]:
+                    h_dist += 1
+            for bit in y_bin[len(x_bin):]:
+                if bit == '1':
+                    h_dist += 1
+        else:
+            for idx, bit in enumerate(y_bin):
+                if x_bin[idx] != y_bin[idx]:
+                    h_dist += 1
+            for bit in x_bin[len(y_bin):]:
+                if bit == '1':
+                    h_dist += 1
+
+        return h_dist

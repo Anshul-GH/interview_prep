1887e6f4ac34c53b10b620b4b235076c59e9fde4
Anshul Jain <samgalacestyle01@gmail.com>
2022-05-31 17:53:21 +0530

    Routine Commit

---
+++ interviewer/EPAM/unit_testing.py
@@ -0,0 +1,11 @@
+import unittest
+
+class TestingSum(unittest.TestCase):
+    def test_sum(self):
+        self.assertEqual(sum([2,3,5]), 10, "Sum should be 10.")
+    
+    def test_sum_tuple(self):
+        self.assertEqual(sum([2,3,5]), 10, "Sum should be 10.")
+
+if __name__ == "__main__":
+    unittest.main()

360fb003281c01a7ebc9d8e9db553a50a03afe48
personal <samgalacestyle01@gmail.com>
2022-10-19 14:43:41 +0530

    Routine Commit

--- interviewee/99_employers/octopus/mysite/main/urls.py
+++ interviewee/99_employers/octopus/mysite/main/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index"),
-    path("view1/", views.v1, name="view1")
+    path("", views.home, name="home"),
+    path("<int:id>", views.item, name="item"),
 ]
--- interviewee/99_employers/octopus/mysite/main/views.py
+++ interviewee/99_employers/octopus/mysite/main/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from .models import ToDoList, Item
 # Create your views here.
 
 
@@ -8,5 +8,10 @@ def index(response):
     return HttpResponse("<h1>Learning Django (TWT)</h1>")
 
 
-def v1(response):
-    return HttpResponse("<h1>View 1</h1>")
+def item(response, id):
+    ls = ToDoList.objects.get(id=id)
+    item = ls.item_set.get(id=1)
+    return HttpResponse("<h1>%s</h1><br></br><p>%s</p>" % (ls.name, item.text))
+
+def home(response):
+    pass

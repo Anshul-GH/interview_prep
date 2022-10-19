1552996a9b292b5ce0308eb7332d1b15c2c6dd49
personal <samgalacestyle01@gmail.com>
2022-10-19 16:26:56 +0530

    Routine Commit

--- interviewee/99_employers/octopus/mysite/main/views.py
+++ interviewee/99_employers/octopus/mysite/main/views.py
@@ -5,7 +5,9 @@ from .models import ToDoList, Item
 
 
 def index(response):
-    return HttpResponse("<h1>Learning Django (TWT)</h1>")
+    # return HttpResponse("<h1>Learning Django (TWT)</h1>")
+    ls = ToDoList.objects.get(id=id)
+    return render(response, "main/list.html", {"name": ls.name})
 
 
 def item(response, id):
@@ -13,5 +15,6 @@ def item(response, id):
     item = ls.item_set.get(id=1)
     return HttpResponse("<h1>%s</h1><br></br><p>%s</p>" % (ls.name, item.text))
 
+
 def home(response):
-    pass
+    return render(response, "main/home.html", {"name": "test"})

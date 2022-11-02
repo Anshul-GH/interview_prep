275f0e610008d67888eefbc96f41033fbdd623dd
personal <samgalacestyle01@gmail.com>
2022-11-02 15:42:06 +0530

    Routine Commit

--- interviewee/05_leetcode/solutions/m_longst_substr_wo_repetition.py
+++ interviewee/05_leetcode/solutions/m_longst_substr_wo_repetition.py
@@ -8,11 +8,17 @@ class Solution:
         for chr in s:            
             if chr not in sstr:
                 sstr.append(chr)
+                if ss_len < len(sstr):
+                    ss_len = len(sstr)
             elif len(sstr) > ss_len:
                 ss_len = len(sstr)
                 # find the index of the duplicate char
                 idx = sstr.index(chr)
-                sstr = sstr[1:]
+                if idx < len(sstr):
+                    sstr = sstr[idx+1:]
+                    sstr.append(chr)
+                else:
+                    sstr = []
 
         if ss_len == 0:
             ss_len = len(sstr)

34f3c7c6ec362d193f2f305682c6b264c2caad12
personal <samgalacestyle01@gmail.com>
2022-11-11 01:51:46 +0530

    Routine Commit

---
+++ interviewee/23_rust/variables/src/main.rs
@@ -0,0 +1,28 @@
+fn main() {
+    // let mut x = 4;
+    // println!("x is: {}", x);
+    // x = 5;
+    // println!("x is: {}", x);
+
+
+    // let x = 4;
+    // println!("x is: {}", x);
+    // {
+    //     let x = x - 2;
+    //     println!("x is: {}", x);        
+    // }
+    // let x = x+1;
+    // println!("x is: {}", x);
+
+    // let x = 4;
+    // println!("x is: {}", x);
+
+    // let x = "Hello!";
+    // println!("x is: {}", x);
+
+    // constants
+    const SECONDS_IN_MINUTE: u32 = 60;
+    // const SECONDS_IN_MINUTE: u32 = 100;
+    println!("{}", SECONDS_IN_MINUTE);
+
+}

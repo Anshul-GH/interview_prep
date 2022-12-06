a5a02c588dad192040c4acfc40b93404f9b793f9
personal <samgalacestyle01@gmail.com>
2022-12-06 22:12:37 +0530

    Routine Commit

---
+++ interviewee/05_leetcode/solutions/m_divide_two_integers.py
@@ -0,0 +1,34 @@
+# https://leetcode.com/problems/divide-two-integers/
+
+class Solution:
+    def divide(self, dividend: int, divisor: int) -> int:
+        quotient = 0
+        negative = False
+
+        if dividend < 0:
+            negative = True
+            dividend = -dividend
+        if divisor < 0:
+            if negative:
+                negative = False
+            else:
+                negative = True
+            divisor = -divisor
+        if dividend == divisor:
+            quotient = 1
+        elif dividend < divisor:
+            quotient = 0
+        else:
+            while dividend > divisor:
+                dividend = dividend - divisor
+                quotient += 1
+
+        if negative:
+            quotient = -quotient
+
+        if quotient > (2**31-1):
+            quotient = (2**31-1)
+        elif quotient < -(2**31):
+            quotient = -(2**31)
+
+        return quotient

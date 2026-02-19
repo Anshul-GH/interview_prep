cee20af7d543d8357ce3872bbb82248d74354324
Anshul-GH <samgalacestyle01@gmail.com>
2026-02-19 16:14:30 -0500

    Learning C# again.

--- interviewee/21_docker_kubernetes/L07-02 Multi Stage Build/obj/Debug/net6.0/Code.AssemblyInfo.cs
+++ interviewee/21_docker_kubernetes/L07-02 Multi Stage Build/obj/Debug/net6.0/Code.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Code")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eb94b3381896f98f108fa847bc11f82257d346f3")]
 [assembly: System.Reflection.AssemblyProductAttribute("Code")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Code")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

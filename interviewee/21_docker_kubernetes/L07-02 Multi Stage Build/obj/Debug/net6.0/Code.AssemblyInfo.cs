f777ce8b66652eef184a39d5ac930470e6d670dc
Anshul-GH <samgalacestyle01@gmail.com>
2026-03-19 19:22:30 -0400

    Completed MCP course.

--- interviewee/21_docker_kubernetes/L07-02 Multi Stage Build/obj/Debug/net6.0/Code.AssemblyInfo.cs
+++ interviewee/21_docker_kubernetes/L07-02 Multi Stage Build/obj/Debug/net6.0/Code.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Code")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e20ba0907eb2f465c3b8f91a603f10efe3ef50bf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+54252e57c3a88615399cffd5ce6dfc62f56d4209")]
 [assembly: System.Reflection.AssemblyProductAttribute("Code")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Code")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
